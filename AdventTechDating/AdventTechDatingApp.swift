cf5bf279885f25754da6600eb58b163530935771
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2025-01-16 16:18:10 -0600

    Initial Commit

---
+++ AdventTechDating/AdventTechDatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventTechDatingApp.swift
+//  AdventTechDating
+//
+//  Created by Irvens Dupuy on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AdventTechDatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventTechDatingTests/AdventTechDatingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdventTechDatingTests.swift
+//  AdventTechDatingTests
+//
+//  Created by Irvens Dupuy on 1/16/25.
+//
+
+import Testing
+@testable import AdventTechDating
+
+struct AdventTechDatingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
